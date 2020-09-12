6463a531d950d7b444abffa6c29d6cebb97e20cb
askaborg <hienle@askaborg.com>
2020-09-11 18:25:48 -0700

    oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class
+class Vehicle:
+    pass
+
+# Vehicle subclass
+class GroundVehicle(Vehicle):
+    pass
+
+# GroundVehicle subclass
+class Car(GroundVehicle):
+    pass
+
+# GroundVehicle subclass
+class Motorcycle(GroundVehicle):
+    pass
+
+# Vehicle subclass
+class FlightVehicle(Vehicle):
+    pass
+
+# FlightVehicle subclass
+class Airplane(FlightVehicle):
+    pass
+
+# FlightVehicle subclass
+class Starship(FlightVehicle):
+    pass
